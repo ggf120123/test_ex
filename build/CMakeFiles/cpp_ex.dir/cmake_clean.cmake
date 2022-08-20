a8b89d01dd6adbb961b6a435deb3180152507156
ggf120123 <ggf120@gmail.com>
2022-08-20 09:12:27 +0800

    first commit

---
+++ build/CMakeFiles/cpp_ex.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_ex.dir/main.cpp.obj"
+  "CMakeFiles/cpp_ex.dir/main.cpp.obj.d"
+  "cpp_ex.exe"
+  "cpp_ex.exe.manifest"
+  "cpp_ex.pdb"
+  "libcpp_ex.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_ex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/ggWorkspace/testArea/vscodes/cpp_project
+# Build directory: D:/ggWorkspace/testArea/vscodes/cpp_project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
